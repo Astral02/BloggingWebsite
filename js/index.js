0f98e828605ee88785617eb03a46530702c76396
emansi <mansielhance@gmail.com>
2019-06-26 17:52:56 +0530

    Fourth Commit: Modal completed for 'Create Post' button.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,25 @@
 function openBlogList(){
   window.open("html/bloglist.html");
 }
+
+createPostModalFunction();
+
+//'Create post' modal
+function createPostModalFunction(){
+  var modal = document.getElementById("createPostModal");
+  var createPostBtnOpen = document.getElementById("createPostButton");
+  var createPostBtnClose = document.getElementsByClassName("ModalClose")[0];
+
+  createPostBtnOpen.onclick = function() {
+    modal.style.display = "block";
+  }
+  createPostBtnClose.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+}
